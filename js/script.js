098c24353cf1637a5640163e7cb2e4b427df940b
StanislauS <stanislauskopenko@gmail.com>
2019-01-28 08:42:32 +0300

    валидация формы

--- js/script.js
+++ js/script.js
@@ -14,6 +14,22 @@
   var switchs = sliders.querySelectorAll('.switch');
   var sliderItems = sliders.querySelectorAll('.slider-item');
   var body = document.querySelector('.index');
+  var subscriptionForm = document.querySelector('.subscription-form');
+  var subscriptionEmail = document.getElementById('email');
+  var subscriptionButton = subscriptionForm.querySelector('.btn-form');
+
+  var checkForm = function (evt) {
+    if(!subscriptionEmail.value) {
+      evt.preventDefault();
+      subscriptionEmail.setCustomValidity('Это поле не должно быть пустым');
+    } else if (subscriptionEmail.value) {
+      var array = subscriptionEmail.value.split('@');
+      if(!(array[1].indexOf('.') + 1)){
+        evt.preventDefault();
+        subscriptionEmail.setCustomValidity('Введите корректный адресс, после символа "@" должна присутствовать точка');
+      }
+    }
+  };
 
 //переключение слайдов
   var makeSwitchSlider = function () {
@@ -22,7 +38,7 @@
       if(el.classList.contains('switch-active')) {
         el.classList.remove('switch-active');
         sliderItems[index].classList.remove('slider-active');
-        body.classList.remove('slider' + (index + 1));
+        body.classList.remove('bg' + (index + 1));
         if(index === (array.length - 1)) {
           index = -1;
         }
@@ -31,7 +47,7 @@
     })
     switchs[number + 1].classList.add('switch-active');
     sliderItems[number + 1].classList.add('slider-active');
-    body.classList.add('slider' + (number + 2));
+    body.classList.add('bg' + (number + 2));
   };
 
 //переключение слайдов по нажатию кнопок
@@ -40,13 +56,13 @@
     [].forEach.call(switchs, function(el, index) {
       el.classList.remove('switch-active');
       sliderItems[index].classList.remove('slider-active');
-      body.classList.remove('slider' + (index + 1));
+      body.classList.remove('bg' + (index + 1));
     });
     evt.target.classList.add('switch-active');
     [].forEach.call(switchs, function(el, index) {
       if(el.classList.contains('switch-active')) {
         sliderItems[index].classList.add('slider-active');
-        body.classList.add('slider' + (index + 1));
+        body.classList.add('bg' + (index + 1));
       }
     })
   };
@@ -102,6 +118,10 @@
 
   setInterval(makeSwitchSlider, 5000);
 
+  subscriptionEmail.addEventListener('input', function () {
+    subscriptionEmail.setCustomValidity('');
+  });
+  subscriptionForm.addEventListener('submit', checkForm)
   buttonOopen.addEventListener('click', openFeedback);
   buttonOopen.addEventListener('keydown', openPopup);
   [].forEach.call(switchs, function(el) {
