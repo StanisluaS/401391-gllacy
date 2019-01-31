a5fa90a44de3b7f5eba1547768eb83f062276ce0
StanislauS <stanislauskopenko@gmail.com>
2019-01-31 03:37:24 +0300

    сделал

--- js/addProduct.js
+++ js/addProduct.js
@@ -7,34 +7,68 @@
   var basket = document.querySelector('.basket');
   var basketLink = basket.querySelector('.basket-link');
   var amountProduct = basket.querySelector('.amount-product');
+  var basketProduct = basket.querySelectorAll('.basket-product');
+  var buttonsDelete = basket.querySelectorAll('.btn-delete') || null;
+
+  var addWrapperList = function () {
+    var basketWrapperTemplate = document.querySelector('#basket-wrapper-template').content.cloneNode(true);
+    basket.appendChild(basketWrapperTemplate);
+    basket.classList.add('basket-full');
+    basketLink.classList.add('basket-link-full');
+  };
+
+  var addDataProduct = function (picture, name, price) {
+    var basketList = basket.querySelector('.basket-list');
+	  var totalBasketPrice = basket.querySelector('.basket-sum');
+	  var sum = +totalBasketPrice.textContent.replace(' руб.', '');
+    var basketProductTemplate = document.querySelector('#basket-product-template').content.cloneNode(true);
+  	var productBasketPicture = basketProductTemplate.querySelector('img');
+	  var productBasketName = basketProductTemplate.querySelector('.product-name').children[0];
+	  var productBasketPrice = basketProductTemplate.querySelector('.price-kilo');
+	  var productBasketTotalPrice = basketProductTemplate.querySelector('.price-total');
+    var productButtonDelete = basketProductTemplate .querySelector('.btn-delete');
+
+    productBasketPicture.setAttribute('src', picture);
+	  productBasketName.textContent = name;
+	  productBasketPrice.textContent = price + ' руб';
+	  productBasketTotalPrice.textContent = 1.5 * price + ' руб.';
+	  totalBasketPrice.textContent = sum + 1.5 * price + ' руб.';
+    productButtonDelete.addEventListener('click', removeProduct);
+    basketList.appendChild(basketProductTemplate);
+  };
 
   var removeProduct = function (evt) {
 	  evt.preventDefault();
+    var totalBasketPrice = basket.querySelector('.basket-sum');
+	  var sum = +totalBasketPrice.textContent.replace(' руб.', '');
+    var sumProduct = +amountProduct.textContent.split(' ')[0];
 	  var target = evt.target;
-  	var el = target;
+    var parent = target.parentElement;
+    var price = null;
+
+    sumProduct -= 1;
+    if(sumProduct === 1) {
+      amountProduct.textContent = sumProduct + ' товар';
+    } else if(sumProduct > 1 && sumProduct < 5) {
+      amountProduct.textContent = sumProduct + ' товара';
+    } else if(sumProduct >= 5){
+      amountProduct.textContent = sumProduct + ' товаров';
+    } else {
+      document.querySelector('.basket-wrapper').remove();
+      basket.classList.remove('basket-full');
+      basketLink.classList.remove('basket-link-full');
+      amountProduct.textContent = 'Пусто';
+    }
+
 	  while(true) {
-	    el = el.parentElement;
-	    if(el.parentElement.className === 'basket-product') {
-		    el.parentElement.remove();
-		    var textAmout = amountProduct.textContent.split(' ');
-		    var sumProduct = textAmout[0] - 1;
-		    if(sumProduct === 1) {
-		      amountProduct.textContent = sumProduct + ' товар';
-		    } else if(sumProduct > 1 && sumProduct < 5) {
-		      amountProduct.textContent = sumProduct + ' товара';
-		    } else {
-		      amountProduct.textContent = sumProduct + ' товаров';
-		    }
-	    	var basketProduct = basket.querySelectorAll('.basket-product')
-	  	  if(!basketProduct.length) {
-		      document.querySelector('.basket-wrapper').remove();
-		      basket.classList.remove('basket-full');
-		      basketLink.classList.remove('basket-link-full');
-		      amountProduct.textContent = 'Пусто';
-		    }
+      parent = parent.parentElement;
+      if(parent.className === 'basket-product') {
+        price = parent.querySelector('.price-total').textContent.replace(' руб.', '');
+        totalBasketPrice.textContent = sum - price + ' руб.';
+        parent.remove();
         return;
-	    }
-  	}
+      }
+	  }
   };
 
   var addProduct = function (evt) {
@@ -42,45 +76,26 @@
 	var parentElement = evt.target.parentElement;
 	var pictureProduct = parentElement.querySelector('img').getAttribute('src');
 	var nameProduct = parentElement.querySelector('.product-title').textContent;
-	var priceProduct = parentElement.querySelector('.product-price').textContent.replace('₽/кг', '');	
-    if(!basket.classList.contains('basket-full')) {
-      var basketWrapperTemplate = document.querySelector('#basket-wrapper-template').content.cloneNode(true);
-	    basket.appendChild(basketWrapperTemplate);
-      basket.classList.add('basket-full');
-	    basketLink.classList.add('basket-link-full');
-  	}
-    var basketList = basket.querySelector('.basket-list');
-	var totalBasketPrice = basket.querySelector('.basket-sum');
-	var sum = +totalBasketPrice.textContent.replace(' руб.', '');
-    var basketProductTemplate = document.querySelector('#basket-product-template').content.cloneNode(true);
-	var productBasketPicture = basketProductTemplate.querySelector('img');
-	var productBasketName = basketProductTemplate.querySelector('.product-name').children[0];
-	var productBasketPrice = basketProductTemplate.querySelector('.price-kilo');
-	var productBasketTotalPrice = basketProductTemplate.querySelector('.price-total');
-	productBasketPicture.setAttribute('src', pictureProduct);
-	productBasketName.textContent = nameProduct;
-	productBasketPrice.textContent = priceProduct + ' руб';
-	productBasketTotalPrice.textContent = 1.5 * priceProduct + ' руб.';
-	totalBasketPrice.textContent = sum + 1.5 * priceProduct + ' руб.';
-    basketList.appendChild(basketProductTemplate);
-    var buttonsDelete = basketList.querySelectorAll('.btn-delete');
-    [].forEach.call(buttonsDelete, function(el) {
-      el.addEventListener('click', removeProduct);
-    })
+	var priceProduct = parentElement.querySelector('.product-price').textContent.replace('₽/кг', '');
+  var sumProduct = +amountProduct.textContent.split(' ')[0] || 0;
 
-	  var textAmout = +amountProduct.textContent.split(' ')[0] || 0;
-	  var sumProduct = textAmout + 1;
-	  if(sumProduct === 1) {
-		  amountProduct.textContent = sumProduct + ' товар';
-	  } else if(sumProduct > 1 && sumProduct < 5) {
-		  amountProduct.textContent = sumProduct + ' товара';
-  	} else {
-		  amountProduct.textContent = sumProduct + ' товаров';
-	  }
-
-
+  sumProduct += 1;
+  if(sumProduct === 1) {
+    addWrapperList();
+    amountProduct.textContent = sumProduct + ' товар';
+  } else if(sumProduct > 1 && sumProduct < 5) {
+    amountProduct.textContent = sumProduct + ' товара';
+  } else {
+    amountProduct.textContent = sumProduct + ' товаров';
+  }
+    addDataProduct(pictureProduct, nameProduct, priceProduct);
   };
 
+  if(buttonsDelete) {
+    [].forEach.call(buttonsDelete, function(el){
+    	el.addEventListener('click', removeProduct);
+    });
+  }
 
   [].forEach.call(buttonsChoice, function(el){
   	el.addEventListener('click', addProduct);
